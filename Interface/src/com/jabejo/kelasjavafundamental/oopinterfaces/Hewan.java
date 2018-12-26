657022d8993d1d73e84fcfc46d365252199a9a75
Moh Arif Jauhari <moh.arif.jauhari@gmail.com>
2018-12-26 18:08:55 +0700

    Interface dan Implement
    Kelas Java Fundamental
    Topik Interface dan Implement

---
+++ Interface/src/com/jabejo/kelasjavafundamental/oopinterfaces/Hewan.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.jabejo.kelasjavafundamental.oopinterfaces;
+
+/**
+ * @author jabejo13
+ *
+ */
+public interface Hewan {
+	String RESPIRASI = "oksigen";
+	void makan();
+	
+}
