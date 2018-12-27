066b33d3e195e2b75c9c2778bba00c071d3ede48
Moh Arif Jauhari <moh.arif.jauhari@gmail.com>
2018-12-27 17:41:01 +0700

    Quiz

--- Quiz/src/com/jabejo/kelasjavafundamental/quiz/Quiz.java
+++ Quiz/src/com/jabejo/kelasjavafundamental/quiz/Quiz.java
@@ -3,17 +3,18 @@ package com.jabejo.kelasjavafundamental.quiz;
 public class Quiz {
 
 	public static void main(String[] args) {
-		int x = 5;
-		if (x > 6)
-			System.out.println("x>i");
-		else if (x >= 5)
-			System.out.println("x>=5");
-		else if (x < 10)
-			System.out.println("x<10");
-		else
-			System.out.println("bukan dari salah satu di atas");
-
+		// int x = 5;
+		// if (x > 6)
+		// System.out.println("x>i");
+		// else if (x >= 5)
+		// System.out.println("x>=5");
+		// else if (x < 10)
+		// System.out.println("x<10");
+		// else
+		// System.out.println("bukan dari salah satu di atas");
+		String s1 = "geeksquiz";
+		String s2 = "geeksquiz";
+		System.out.println("s1 == s2 is:" + String.valueOf(s1 == s2));
 	}
 
 }
-	
